3a82eae002de16f9a6aecfe8e416eac3d4ccbe8e
BigProject404 <bigproject404@yandex.ru>
2021-10-03 00:42:24 +0300

    token deleted

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 settings = {
-    'token': 'ODg5NTA5Mjk0NTM2NTQ0MjY2.YUiSFg.oVv-hEkQFKtq0CI5MiTxWkaqSEo', # Token was deleted due to the safety measures
+    'token': '', # Token was deleted due to the safety measures
     'bot': 'LiprikonBoot',
-    'id': 889509294536544266,
+    'id': ,
     'prefix': '/'
 }
