f32fd11a6c76946eca45a60605c97c57a4c837fc
BigProject404 <bigproject404@yandex.ru>
2021-10-03 00:39:14 +0300

    more funcs

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 settings = {
-    'token': '<string here>', # Token was deleted due to the safety measures
+    'token': 'ODg5NTA5Mjk0NTM2NTQ0MjY2.YUiSFg.oVv-hEkQFKtq0CI5MiTxWkaqSEo', # Token was deleted due to the safety measures
     'bot': 'LiprikonBoot',
     'id': 889509294536544266,
     'prefix': '/'
--- main.py
+++ main.py
@@ -5,8 +5,35 @@ from config import settings
 bot = commands.Bot(command_prefix = settings['prefix'])
 
 @bot.command()
+async def салам(ctx):
+    author = ctx.message.author
+    await ctx.send(f'Салам алейкум, {author.mention}!')
+
+@bot.command()
 async def hello(ctx):
     author = ctx.message.author
     await ctx.send(f'Hello, {author.mention}!')
+
+
+@bot.command()
+async def правила(ctx):
+    await ctx.send(f'1.Не спамить')
+    await ctx.send(f'2.Не бунтовать')
+    await ctx.send(f'3.Не оскорблять админов и других участников')
+    await ctx.send(f'4.Быть вежливым')
+    await ctx.send(f'5.Не менять название сервера без разришения верховного админа')
+    await ctx.send(f'6.Не банить участников без ведома верховного админа')
+    await ctx.send(f'7.Не устраивать революции')
+
+@bot.command()
+async def rules(ctx):
+    await ctx.send(f'1.Не спамить')
+    await ctx.send(f'2.Не бунтовать')
+    await ctx.send(f'3.Не оскорблять админов и других участников')
+    await ctx.send(f'4.Быть вежливым')
+    await ctx.send(f'5.Не менять название сервера без разришения верховного админа')
+    await ctx.send(f'6.Не банить участников без ведома верховного админа')
+    await ctx.send(f'7.Не устраивать революции')
+
 
 bot.run(settings['token'])
