d6046750c23d0f50b469b4844b6c27277c265a6d
BigProject404 <bigproject404@yandex.ru>
2021-10-03 11:23:49 +0300

    new features

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 settings = {
-    'token': '', # Token was deleted due to the safety measures
+    'token': 'ODg5NTA5Mjk0NTM2NTQ0MjY2.YUiSFg.oVZ--7tu_qMuqx8XzycpI9hVkms', # Token was deleted due to the safety measures
     'bot': 'LiprikonBoot',
-    'id': ,
-    'prefix': '/'
+    'id': 889509294536544266,
+    'prefix': '-'
 }
