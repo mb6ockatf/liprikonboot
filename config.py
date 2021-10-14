f80ac5b57420cec2f876a36de8ab0b561111420e
BigProject404 <bigproject404@yandex.ru>
2021-10-14 20:42:39 +0300

    swearing achtung

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 settings = {
     'token': '', # Token was deleted. Fill in your own value to run the code
     'bot': 'LiprikonBoot',
-    'id': 0, # id was deleted. Fill in your own value to run the code
+    'id': , # id was deleted. Fill in your own value to run the code
     'prefix': '/'
 }
