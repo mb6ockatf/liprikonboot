4325b4de8ce5e793feb39ce573887dbb8789c441
BigProject404 <bigproject404@yandex.ru>
2021-10-31 21:25:34 +0300

    Update bot_config.py

--- bot_config.py
+++ bot_config.py
@@ -7,7 +7,7 @@ ds_client_id = os.environ['ds_client_id']
 ds_server_token = os.environ['ds_server_token']
 
 # List of administration roles id's
-ds_server_admins = int(os.environ['ds_server_admins'])  # Moderator's role
+ds_server_admins = os.environ['ds_server_admins']  # Moderator's role
 ds_server_host = os.environ['ds_server_host']  # Server host's  role
 
 ds_bot_name = os.environ['ds_bot_name']
