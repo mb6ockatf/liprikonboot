edf1599bfb04fd00c969bd43d7bebd1b1622597c
BigProject404 <bigproject404@yandex.ru>
2021-11-01 12:22:01 +0300

    fixed

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ from bot_config import (ds_app_token, ds_server_admins,
 client = discord.Client()
 bot = commands.Bot(command_prefix=ds_prefix)
 
-def right_server(ctx=ctx):
-    if ctx.message.guild.id == myserver:
+def right_server(ctx):
+    if ctx.guild.id == myserver:
         return True
     else:
         return False
 
-def is_admin(ctx=ctx):
+def is_admin(ctx=discord.ext.commands.Context):
     role = ctx.guild.get_role(role_id=ds_server_admins)
     if role in ctx.message.author.roles:
         return True
@@ -37,8 +37,8 @@ class System(commands.Cog):
          <prefix here>clear
         - deletes all messages
         """
-        if right_server():
-            if is_admin():
+        if right_server(ctx):
+            if is_admin(ctx):
                 await ctx.channel.purge(limit=amount)
                 await ctx.send(':white_check_mark: Successfully deleted.')
             else:
@@ -59,7 +59,7 @@ class System(commands.Cog):
         <сдесь префикс>очистить
         - удалит все сообщения
         """
-        if right_server():
+        if right_server(ctx):
             if is_admin(ctx):
                 await ctx.channel.purge(limit=amount)
                 await ctx.send(':white_check_mark: Сообщения успешно удалены.')
@@ -71,7 +71,7 @@ class Ping(commands.Cog):
     @commands.command()
     async def салам(self, ctx):
         """RU: Проверка, онлайн ли бот"""
-        if right_server():
+        if right_server(ctx):
             author = ctx.message.author
             await ctx.send(f'Салам алейкум, {author.mention}!')
             await asyncio.sleep(5)
@@ -80,7 +80,7 @@ class Ping(commands.Cog):
     @commands.command()
     async def hello(self, ctx):
         """Check if the bot is online"""
-        if right_server():
+        if right_server(ctx):
             author = ctx.message.author
             await ctx.send(f'Hello, {author.mention}!')
             await asyncio.sleep(5)
@@ -92,7 +92,7 @@ class Information(commands.Cog):
     async def правила(self, ctx):
         """RU: Правила"""
         # TODO: it must send a message which id is defined in env vars
-        if right_server():
+        if right_server(ctx):
             await ctx.send('1.Не спамить\n'
                            '2.Не бунтовать\n'
                            '3.Не оскорблять админов и других \
@@ -111,7 +111,7 @@ class Information(commands.Cog):
     async def rules(self, ctx):
         """Shows the rules"""
         # TODO: it must send a message which id is defined in env vars
-        if right_server():
+        if right_server(ctx):
             await ctx.send('1.Do not spam\n'
                            '2.Do not rebel\n'
                            '3.Do not offend admins & other members\n'
@@ -128,7 +128,7 @@ class Information(commands.Cog):
     @commands.command(pass_context=True)
     async def префикс(self, ctx):
         """RU: Текущий префикс"""
-        if right_server():
+        if right_server(ctx):
             await ctx.send(ds_prefix)
             await asyncio.sleep(5)
             await ctx.message.delete()
@@ -136,7 +136,7 @@ class Information(commands.Cog):
     @commands.command(pass_context=True)
     async def prefix(self, ctx, amount=1):
         """Current prefix"""
-        if right_server():
+        if right_server(ctx):
             await ctx.send(ds_prefix)
             await asyncio.sleep(5)
             await ctx.message.delete()
@@ -146,26 +146,26 @@ class Mention(commands.Cog):
     @commands.command()
     async def pingadmins(self, ctx: commands.Context):
         """Pings admin role"""
-        if right_server():
+        if right_server(ctx):
             role = ctx.guild.get_role(role_id=ds_server_admins)
             await ctx.message.reply("Achtung!" + f"{role.mention}" + f"were mentioned")
 
     @commands.command()
     async def куадмины(self, ctx: commands.Context):
         """RU: Упоминание модераторов"""
-        if right_server():
+        if right_server(ctx):
             role = ctx.guild.get_role(role_id=ds_server_admins)
             await ctx.message.reply("Ахтунг!" + f"{role.mention}" + "были упомянуты")
 
 
 @bot.event
-async def on_message(message):
+async def on_message(ctx):
     # React when message contains swearing
-    if right_server():
+    if right_server(ctx):
         for j in swearing:
-            if j in message.content:
-                await message.reply('pong', mention_author=True)
-        await bot.process_commands(message)
+            if j in ctx.content:
+                await ctx.reply('pong', mention_author=True)
+        await bot.process_commands(ctx)
 
 
 bot.add_cog(System())
