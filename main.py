109c8e02e6573cf64bc8102d489be13c99b909bb
BigProject404 <bigproject404@yandex.ru>
2022-05-02 19:33:42 +0300

    A

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 from sys import argv
-from subprocess import check_output
+from os import system
 from datetime import datetime as dt
 
 start = dt.now()
@@ -12,16 +12,20 @@ client = discord.Client(max_messages=10)
 
 @client.event
 async def on_message(message):
-    author, content = message.author, message.content
+    author, content = message.author, message.content.split()
+    command = content[0]
     if author == client.user:
         return
     if author.id == 725303688192720977:
-        if content == 'ping':
+        if command == 'ping':
             await message.channel.send('pong')
-        elif content == 'uptime':
+        elif command == 'uptime':
             await message.channel.send(dt.now() - start)
-        elif content.startswith('execute'):
-            await message.channel.send(check_output(content[7:], universal_newlines=True))
+        elif command == 'log':
+            b = '\n'
+            system(f'echo {" ".join(content[1:]) + b} >> ../logging.txt')
+        elif command == 'system':
+            await message.channel.send(system(" ".join(content[1:])))
 
 
 client.run(TOKEN)
