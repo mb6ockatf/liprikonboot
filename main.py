06a544b7ed08ffa063b62fdebfea349f230f0d49
BigProject404 <bigproject404@yandex.ru>
2022-05-06 18:18:24 +0300

    Resolve conflict

--- main.py
+++ main.py
@@ -1,32 +1,29 @@
-import discord
-from discord.ext import commands
+from discord import Client
 from sys import argv
 from os import system
 from datetime import datetime as dt
 
-start = dt.now()
-prefix = '/'
-TOKEN = argv[1]
-client = discord.Client(max_messages=10)
+client, start = Client(max_messages=10), dt.now()
 
 
 @client.event
 async def on_message(message):
-    author, content = message.author, message.content.split()
-    command = content[0]
-    if author == client.user:
+    author, content = message.author, " ".join(message.content.split())
+    command = content[0:content.find(' ')]
+    if author in (client.user, 725303688192720977):
         return
-    if author.id == 725303688192720977:
-        if command == 'ping':
-            await message.channel.send('pong')
-        elif command == 'uptime':
-            await message.channel.send(dt.now() - start)
-        elif command == 'log':
-            b = '\n'
-            system(f'echo "{" ".join(content[1:])}" >> ../logging.txt')
-        elif command == 'system':
-            system(f" {' '.join(content[1:])} > temp.txt")
-            await message.channel.send(open('temp.txt', 'r').read())
+    if command == '$ping':
+        await message.channel.send('pong')
+    elif command == '$uptime':
+        await message.channel.send(dt.now() - start)
+    else:
+        line = " ".join(content[1:])
+        if command == '$log':
+            system('echo ' + line + '>> ../logging.txt')
+        elif command == '$$':
+            system(line + ' > temp.txt')
+            with open('temp.txt', 'r') as file:
+                await message.channel.send(file.read())
 
 
-client.run(TOKEN)
+client.run(argv[1])
